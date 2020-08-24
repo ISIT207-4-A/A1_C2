1c46d6e5f4a78911e40e9a330498e859606ff7d7
ngvh2605 <48985636+ngvh2605@users.noreply.github.com>
2020-08-24 19:16:51 +1000

    Update 1_e.js

--- 1_e.js
+++ 1_e.js
@@ -1,7 +1,4 @@
 let array = ['abcdefghi', 'abctki', 'tb21eT', '871', 'T4285T', 'ptxtxtxtxXYZuy', 'ept223edt', 'ba329', 'trtedtcxyzu'];
-let array_size_6 = array.filter(word => word.length == 6);
-console.log(array_size_6);
-
 
 var regex_i = /^t....t$/i;
 var regex_ii = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]{3,7})$/;
